ee996480ed2ad3a814ce065ee35cdbef1f698ac5
Kantou002 <crazyldg113@gmail.com>
2025-09-19 14:12:48 +0700

    bai1

--- BuoiTH3/Bai1/Form1.Designer.cs
+++ BuoiTH3/Bai1/Form1.Designer.cs
@@ -28,12 +28,116 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            txtUser = new TextBox();
+            txtPass = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            chkNho = new CheckBox();
+            btnDangnhap = new Button();
+            btnXoa = new Button();
+            btnDung = new Button();
+            SuspendLayout();
+            // 
+            // txtUser
+            // 
+            txtUser.Location = new Point(176, 34);
+            txtUser.Name = "txtUser";
+            txtUser.Size = new Size(175, 27);
+            txtUser.TabIndex = 0;
+            // 
+            // txtPass
+            // 
+            txtPass.Location = new Point(176, 74);
+            txtPass.Name = "txtPass";
+            txtPass.Size = new Size(175, 27);
+            txtPass.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(51, 37);
+            label1.Name = "label1";
+            label1.Size = new Size(107, 20);
+            label1.TabIndex = 2;
+            label1.Text = "Tên đăng nhập";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(51, 77);
+            label2.Name = "label2";
+            label2.Size = new Size(70, 20);
+            label2.TabIndex = 3;
+            label2.Text = "Mật khẩu";
+            // 
+            // chkNho
+            // 
+            chkNho.AutoSize = true;
+            chkNho.Location = new Point(57, 109);
+            chkNho.Name = "chkNho";
+            chkNho.Size = new Size(85, 24);
+            chkNho.TabIndex = 4;
+            chkNho.Text = "Ghi Nhớ";
+            chkNho.UseVisualStyleBackColor = true;
+            // 
+            // btnDangnhap
+            // 
+            btnDangnhap.Location = new Point(57, 146);
+            btnDangnhap.Name = "btnDangnhap";
+            btnDangnhap.Size = new Size(94, 29);
+            btnDangnhap.TabIndex = 5;
+            btnDangnhap.Text = "Đăng Nhập";
+            btnDangnhap.UseVisualStyleBackColor = true;
+            btnDangnhap.Click += btnDangnhap_Click;
+            // 
+            // btnXoa
+            // 
+            btnXoa.Location = new Point(157, 146);
+            btnXoa.Name = "btnXoa";
+            btnXoa.Size = new Size(94, 29);
+            btnXoa.TabIndex = 6;
+            btnXoa.Text = "Xóa";
+            btnXoa.UseVisualStyleBackColor = true;
+            btnXoa.Click += btnXoa_Click;
+            // 
+            // btnDung
+            // 
+            btnDung.Location = new Point(257, 146);
+            btnDung.Name = "btnDung";
+            btnDung.Size = new Size(94, 29);
+            btnDung.TabIndex = 7;
+            btnDung.Text = "Dừng";
+            btnDung.UseVisualStyleBackColor = true;
+            btnDung.Click += btnDung_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(417, 187);
+            Controls.Add(btnDung);
+            Controls.Add(btnXoa);
+            Controls.Add(btnDangnhap);
+            Controls.Add(chkNho);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(txtPass);
+            Controls.Add(txtUser);
+            Name = "Form1";
+            Text = "Form Đăng Nhập";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private TextBox txtUser;
+        private TextBox txtPass;
+        private Label label1;
+        private Label label2;
+        private CheckBox chkNho;
+        private Button btnDangnhap;
+        private Button btnXoa;
+        private Button btnDung;
     }
 }
--- BuoiTH3/Bai1/Form1.cs
+++ BuoiTH3/Bai1/Form1.cs
@@ -1,4 +1,4 @@
-namespace Bai1
+﻿namespace Bai1
 {
     public partial class Form1 : Form
     {
@@ -6,5 +6,34 @@ namespace Bai1
         {
             InitializeComponent();
         }
+
+        private void btnDangnhap_Click(object sender, EventArgs e)
+        {
+            string thongbao;
+            thongbao = "Tên đăng nhập là: ";
+            thongbao += this.txtUser.Text;
+            thongbao += "\n\rMật khẩu là:";
+            thongbao += this.txtPass.Text;
+            if (this.chkNho.Checked == true)
+                thongbao += "\n\rBạn có ghi nhớ.";
+            MessageBox.Show(thongbao, "Thông báo");
+        }
+
+        private void btnXoa_Click(object sender, EventArgs e)
+        {
+            this.txtUser.Clear();
+            this.txtPass.Clear();
+            this.txtUser.Focus();
+        }
+
+        private void btnDung_Click(object sender, EventArgs e)
+        {
+            DialogResult traloi;
+            traloi = MessageBox.Show("Bạn có muốn đóng chương trình không?", "Thông báo",
+            MessageBoxButtons.OKCancel, MessageBoxIcon.Question);
+            if (traloi == DialogResult.OK)
+                Application.Exit();
+        }
+        
     }
 }
